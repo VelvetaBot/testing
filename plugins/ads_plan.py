7081c022cbdd1b028aad4b7d682bd3d92e4de242
VelvetaBot <108663991+VelvetaBot@users.noreply.github.com>
2026-04-25 22:00:27 +0530

    Update ads_plan.py

--- plugins/ads_plan.py
+++ plugins/ads_plan.py
@@ -7,9 +7,8 @@ from datetime import datetime, timedelta
 def get_header(user_id):
     user = users_db.find_one({"user_id": user_id}) or {}
     plan = user.get("plan", "FREE")
-    # మొబైల్ స్క్రీన్ కి సరిపోయేలా స్పేస్‌లు
-    if plan == "PREMIUM": return "<blockquote><b>💎 Velveta Premium User </b>ㅤㅤㅤㅤㅤㅤㅤㅤ</blockquote>\n\n"
-    elif plan == "ADS_PREMIUM": return "<blockquote><b>📺 Velveta Semi Premium </b>ㅤㅤㅤㅤㅤㅤ</blockquote>\n\n"
+    if plan == "PREMIUM": return "<blockquote><b>💎 Velveta Premium User                                                                                                                                                                                                        </b></blockquote>"
+    elif plan == "ADS_PREMIUM": return "<blockquote><b> 📺 Velveta Semi Premium User                                                                                                                                                                                                                                                                             </b></blockquote>"
     else: return ""
 
 # ==========================================
